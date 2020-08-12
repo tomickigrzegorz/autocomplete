2a9a9a1290e8780dc5dc0b2285a67bf9ba14e0fd
Grzegorz Tomicki <gtomik@gmail.com>
2020-08-12 10:44:14 +0200

    refactoring

--- sources/js/helpers/hasClass.js
+++ sources/js/helpers/hasClass.js
@@ -1,11 +1,7 @@
 import classListSupport from './classListSupport';
 
 const hasClass = classListSupport
-  ? (el, str) => {
-      return el.classList.contains(str);
-    }
-  : (el, str) => {
-      return el.className.indexOf(str) >= 0;
-    };
+  ? (el, str) => el.classList.contains(str)
+  : (el, str) => el.className.indexOf(str) >= 0;
 
 export default hasClass;

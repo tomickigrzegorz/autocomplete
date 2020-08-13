2f27b6055d33f9f485bb2987b5258e7a83811852
Grzegorz Tomicki <gtomik@gmail.com>
2020-08-13 14:03:53 +0200

    selectFirst - selects the first item of results

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,8 +1,9 @@
-import '../scss/style.scss';
+// import 'promise-polyfill/src/polyfill'; // enable if you want to support IE11
+// import './helpers/element-closest-polyfill.js'; // enable if you want to support IE11
 
-// import 'promise-polyfill/src/polyfill'; // enable if you want to support IE
 import isPromise from './helpers/isPromise';
 import isObject from './helpers/isObject';
+import hasClass from './helpers/hasClass';
 
 class Autosuggest {
   constructor(
@@ -13,6 +14,7 @@ class Autosuggest {
       howManyCharacters,
       instruction,
       onResults,
+      selectFirst,
       onSearch,
       onSubmit = () => { },
     },
@@ -29,6 +31,7 @@ class Autosuggest {
     this.delay = delay || 1000;
     this.howManyCharacters = howManyCharacters || 2;
     this.clearButton = clearButton || false;
+    this.selectFirst = selectFirst || false;
 
     // default config
     this.searchOutputUl = 'autocomplete-list';
@@ -45,12 +48,12 @@ class Autosuggest {
       TAB: 9,
     };
 
-    // set default aria
-    this.setDefault();
-
     this.initialize();
 
     this.createOutputSearch();
+
+    // set default aria
+    this.setDefault();
   }
 
   // default aria
@@ -64,11 +67,14 @@ class Autosuggest {
       '[aria-activedescendant]',
     );
     this.searchId.classList.remove('expanded');
+
+    if (hasClass(this.matchList, this.isActive)) {
+      this.outputSearch.classList.remove(this.isActive);
+    }
   }
 
   initialize = () => {
-    this.createClearButton();
-
+    if (this.clearButton) this.createClearButton();
     let timeout = null;
 
     this.searchId.addEventListener('input', ({ target }) => {
@@ -95,10 +101,12 @@ class Autosuggest {
     this.outputSearch.className = 'auto-output-search';
     this.outputSearch.tabIndex = 0;
     this.outputSearch.setAttribute('role', 'listbox');
+
     this.searchId.parentNode.insertBefore(
       this.outputSearch,
       this.searchId.nextSibling,
     );
+
     this.matchList = document.getElementById(this.searchOutputUl);
     // set instruction
     this.initInstruction();
@@ -111,9 +119,8 @@ class Autosuggest {
 
     // console.log(this.howManny, input.length);
     if (this.howManyCharacters > input.length) {
-      this.hiddenButtonHide();
       this.classSearch.classList.remove(this.isLoading);
-      this.outputSearch.classList.remove(this.isActive);
+      this.hiddenButtonHide();
       this.setDefault();
       return;
     }
@@ -129,7 +136,7 @@ class Autosuggest {
       if (result.length === 0) {
         this.hiddenButtonHide();
         this.searchId.classList.remove('expanded');
-        this.outputSearch.classList.remove(this.isActive);
+
         this.setDefault();
       }
 
@@ -141,19 +148,17 @@ class Autosuggest {
 
   // instruction aria-describedby
   initInstruction = () => {
-    const describedby = document.createElement('span');
-    describedby.id = 'initInstruction';
-    describedby.className = 'init-instruction';
+    this.describedby = document.createElement('span');
+    this.describedby.id = 'initInstruction';
+    this.describedby.className = 'init-instruction';
     this.textContent = document.createTextNode(this.instruction);
-    this.outputSearch.insertAdjacentElement('afterend', describedby);
-    describedby.appendChild(this.textContent);
+    this.outputSearch.insertAdjacentElement('afterend', this.describedby);
+    this.describedby.appendChild(this.textContent);
   }
 
   // hide output div when click on li or press escape
   closeOutputMatchesList = ({ target }) => {
     if (target.id === this.search) return;
-
-    this.outputSearch.classList.remove(this.isActive);
 
     // set default settings
     this.setDefault();
@@ -161,18 +166,19 @@ class Autosuggest {
 
   // preparation of the list
   outputHtml(matches, input) {
+    // set default index
     this.selected = 0;
 
     this.searchId.setAttribute('aria-expanded', true);
     this.searchId.classList.add('expanded');
     this.matchList.innerHTML = this.onResults(matches, input);
 
-    this.searchOutpuli = document.getElementById(this.searchOutputUl);
     this.itemsLi = document.querySelectorAll(`#${this.searchOutputUl} > li`);
 
     this.matchList.classList.add(this.isActive);
 
-    this.hiddenButtonHide();
+    // close expanded items
+    document.addEventListener('click', this.closeOutputMatchesList);
 
     // move with the up / down arrows
     this.searchId.addEventListener('keydown', this.handelEvent);
@@ -197,8 +203,29 @@ class Autosuggest {
       );
     }
 
-    // close expanded items
-    document.addEventListener('click', this.closeOutputMatchesList);
+    // select first li element
+    if (this.selectFirst) {
+      this.selectFirstItem();
+    };
+
+    // hide button clear
+    this.hiddenButtonHide();
+  }
+
+  // select first element
+  selectFirstItem = () => {
+    const { firstElementChild } = document.getElementById(this.searchOutputUl);
+    firstElementChild.id = `${this.selectedOption}-1`;
+    firstElementChild.setAttribute('aria-selected', true);
+    firstElementChild.classList.add(this.activeList);
+
+    this.ariaactivedescendant(
+      this.ariaActivedescendant,
+      `${this.selectedOption}-1`,
+    );
+
+    // set default index
+    this.selected = 1;
   }
 
   hiddenButtonHide = () => {
@@ -208,7 +235,7 @@ class Autosuggest {
 
   // show items when items.length >= 1 and is not empty
   showLiItems = () => {
-    if (this.matchList.innerText.length > 0) {
+    if (this.matchList.textContent.length > 0) {
       this.searchId.setAttribute('aria-expanded', true);
       this.searchId.classList.add('expanded');
       this.outputSearch.classList.add(this.isActive);
@@ -218,50 +245,16 @@ class Autosuggest {
   // adding text from the list when li is clicking
   // or adding aria-selected to li elements
   addTextFromLiToSearchInput = ({ type, target }) => {
-    const activeList = document.querySelector(`.${this.activeList}`);
-
     switch (type) {
       case 'click':
+        this.getTextFromLi(target.closest('li'));
+        // set default index
         this.selected = 0;
-
-        this.firstElementFromLi = target.closest('li');
-
-        if (!this.firstElementFromLi) return;
-
-        this.searchId.value = this.firstElementFromLi.firstElementChild.innerText.trim();
-
-        this.dataElements(this.firstElementFromLi.getAttribute('data-elements'));
-
-        this.outputSearch.classList.remove(this.isActive);
-
-        // set default settings
-        this.setDefault();
-
-        // onSubmit
-        this.onSubmit(
-          this.firstElementFromLi.firstElementChild.innerText.trim(),
-        );
         break;
 
       case 'mouseenter':
-        this.selected = 0;
-        if (activeList) activeList.classList.remove(this.activeList);
-        target.id = this.selectedOption;
-        target.setAttribute('aria-selected', true);
-        target.classList.add(this.activeList);
-        this.ariaactivedescendant(
-          this.ariaActivedescendant,
-          this.selectedOption,
-        );
-        break;
-
       case 'mouseleave':
-        this.selected = 0;
-        if (activeList) activeList.classList.remove(this.activeList);
-        target.id = '';
-        target.setAttribute('aria-selected', false);
-        target.classList.remove(this.activeList);
-        this.ariaactivedescendant(this.ariaActivedescendant);
+        this.mouseentermouseleave(target, type === 'mouseenter');
         break;
 
       default:
@@ -269,12 +262,53 @@ class Autosuggest {
     }
   }
 
-  // the part responsible for appending json to the search
-  // field use - https://github.com/tomik23/Leaflet.Autocomplete
-  dataElements = (item) => {
-    if (!item) return;
-    this.searchId.setAttribute('data-elements', item);
+  // event on mouse
+  mouseentermouseleave = (target, type) => {
+    this.checkActiveListExist = document.querySelector(`.${this.activeList}`);
+    if (this.checkActiveListExist) this.checkActiveListExist.classList.remove(this.activeList);
+
+    target.id = type ? `${this.selectedOption}-${this.indexLiSelected(target)}` : '';
+    target.setAttribute('aria-selected', !!type);
+
+    // add or remove class from selected menu
+    target.classList[type ? 'add' : 'remove'](this.activeList);
+
+    this.ariaactivedescendant(
+      this.ariaActivedescendant,
+      type ? `${this.selectedOption}-${this.indexLiSelected(target)}` : null,
+    );
+
+    // return which li element
+    // was selected and set
+    this.selected = type ? this.indexLiSelected(target) : 0;
   }
+
+  // get text from li on enter or mouseenter
+  getTextFromLi = (element) => {
+    // check if li have children elements
+    this.getText = element.firstElementChild
+      ? element.firstElementChild
+      : element;
+
+    // add text to input
+    this.searchId.value = this.getText.textContent.trim();
+
+    // set default settings
+    this.setDefault();
+
+    // onSubmit passing text to function
+    this.onSubmit(this.getText.textContent.trim());
+
+    // the part responsible for appending json to the search
+    // field use - https://github.com/tomik23/Leaflet.Autocomplete
+    if (element.hasAttribute('data-elements')) {
+      this.dataElements(element.getAttribute('data-elements'));
+    }
+  }
+
+  // return which li element was selected
+  // by hovering the mouse over
+  indexLiSelected = (target) => Array.prototype.indexOf.call(this.itemsLi, target) + 1;
 
   // navigating the elements li and enter
   handelEvent = ({ keyCode }) => {
@@ -288,57 +322,49 @@ class Autosuggest {
         if (this.selected > this.itemsLi.length) {
           this.selected = 1;
         }
-        this.setAriaSelectedItem(this.selected - 1);
-
+        this.setAriaSelectedItem(this.itemsLi[this.selected - 1]);
         break;
       case this.keyCodes.DOWN:
         this.selected -= 1;
         if (this.selected <= 0) {
           this.selected = this.itemsLi.length;
         }
-        this.setAriaSelectedItem(this.selected - 1);
+        this.setAriaSelectedItem(this.itemsLi[this.selected - 1]);
 
         break;
       case this.keyCodes.ENTER:
-        this.searchId.value = this.selectedLi.firstElementChild.innerText.trim();
 
-        this.dataElements(this.selectedLi.getAttribute('data-elements'));
+        this.getTextFromLi(this.selectedLi);
 
-        this.outputSearch.classList.remove(this.isActive);
-
-        this.setDefault();
-
-        // onSubmit
-        this.onSubmit(this.selectedLi.firstElementChild.innerText.trim());
         break;
+
       case this.keyCodes.ESC:
-        this.outputSearch.classList.remove(this.isActive);
 
         this.setDefault();
+
         break;
       default:
         break;
     }
 
-    if (this.selectedLi) this.removeAriaSelectedItem();
+    this.removeAriaSelectedItem();
   }
 
   // set aria label on item li
-  setAriaSelectedItem = (number) => {
-    this.itemsLi[number].id = this.selectedOption;
-    this.itemsLi[number].setAttribute('aria-selected', true);
-    this.itemsLi[number].classList.add(this.activeList);
+  setAriaSelectedItem = (target) => {
+    target.id = `${this.selectedOption}-${this.indexLiSelected(target)}`;
+    target.setAttribute('aria-selected', true);
+    target.classList.add(this.activeList);
 
-    this.ariaactivedescendant(this.ariaActivedescendant, this.selectedOption);
+    this.ariaactivedescendant(this.ariaActivedescendant, `${this.selectedOption}-${this.indexLiSelected(target)}`);
   }
 
   // remove aria label from item li
   removeAriaSelectedItem = () => {
+    if (!this.selectedLi) return;
     this.selectedLi.id = '';
-    this.selectedLi.setAttribute('aria-selected', false);
     this.selectedLi.classList.remove(this.activeList);
-
-    this.ariaactivedescendant(this.ariaActivedescendant);
+    this.selectedLi.setAttribute('aria-selected', false);
   }
 
   // Set aria-activedescendant
@@ -349,8 +375,6 @@ class Autosuggest {
   // create clear button and
   // removing text from the input field
   createClearButton = () => {
-    if (!this.clearButton) return;
-
     this.clearButton = document.createElement('button');
     this.clearButton.id = `auto-clear-${this.search}`;
     this.clearButton.classList.add('auto-clear', 'hidden');
@@ -371,9 +395,16 @@ class Autosuggest {
     // set focus
     this.searchId.focus();
     // remove li from ul
-    this.outputSearch.innerHTML = '';
+    this.outputSearch.textContent = '';
     // set default aria
     this.setDefault();
+  }
+
+  // the part responsible for appending json to the search
+  // field use - https://github.com/tomik23/Leaflet.Autocomplete
+  dataElements = (item) => {
+    if (!item) return;
+    this.searchId.setAttribute('data-elements', item);
   }
 }
 

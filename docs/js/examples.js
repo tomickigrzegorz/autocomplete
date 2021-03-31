bfb1a463309975efb8d7f8c7a830a8b8c12e17b4
Grzegorz Tomicki <gtomik@gmail.com>
2021-03-31 23:15:57 +0200

    doc: new example

--- docs/js/examples.js
+++ docs/js/examples.js
@@ -434,6 +434,43 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
   /**
+   * update input
+   */
+
+  new Autocomplete('update-input', {
+    clearButton: true,
+
+    insertToInput: true,
+
+    onSearch: ({ currentValue }) => {
+      // local data
+      const data = [
+        { "name": "Walter White" },
+        { "name": "Jesse Pinkman" },
+        { "name": "Skyler White" },
+        { "name": "Walter White Jr." }
+      ];
+      return data.sort((a, b) => a.name.localeCompare(b.name))
+        .filter(element => {
+          return element.name.match(new RegExp(currentValue, 'i'))
+        })
+    },
+
+    onResults: ({ matches }) => {
+      return matches
+        .map(el => {
+          return `
+            <li>${el.name}</li>`;
+        }).join('');
+    },
+
+    onSelectedItem: ({ element, object }) => {
+      element.value = object.name
+    }
+  });
+
+
+  /**
    * LOCAL DATA
    */
 

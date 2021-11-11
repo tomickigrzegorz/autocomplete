197d5c698de69aa6dcb0622c8661b24b9f26e420
Grzegorz Tomicki <gtomik@gmail.com>
2021-11-11 18:12:26 +0100

    chore: adding the 'loose' parameter smaller build

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,9 @@ module.exports = {
         // useBuiltIns: 'usage',
         useBuiltIns: 'entry',
         corejs: 3,
+        loose: true,
       },
     ],
   ],
-  plugins: ['@babel/proposal-class-properties'],
+  plugins: [['@babel/proposal-class-properties', { loose: true }]],
 };

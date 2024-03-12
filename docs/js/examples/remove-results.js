513d1828cfbae6fccd65bbbaf335531fa3edd2d1
Grzegorz Tomicki <gtomik@gmail.com>
2024-03-12 13:02:29 +0100

    feat: new method "removeResultsWhenInputIsEmpty"

---
+++ docs/js/examples/remove-results.js
@@ -0,0 +1,23 @@
+new Autocomplete("remove-results", {
+  removeResultsWhenInputIsEmpty: true,
+
+  onSearch: ({ currentValue }) => {
+    const api = `https://rickandmortyapi.com/api/character?name=${encodeURI(
+      currentValue,
+    )}`;
+
+    return new Promise((resolve) => {
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          resolve(data.results);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  },
+
+  onResults: ({ matches }) =>
+    matches.map(({ name }) => `<li>${name}</li>`).join(""),
+});

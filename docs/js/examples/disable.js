219899180e08428ed1b22311ede01d4f3489c23b
Grzegorz Tomicki <gtomik@gmail.com>
2025-09-19 22:46:14 +0200

    feat: add disable method to Autocomplete component
    - Introduced a new `disable` method to the Autocomplete class, allowing users to disable autocomplete functionality while optionally clearing the input value.
    - Updated the TypeScript definition file to include the new `disable` method.
    - Added a new example script demonstrating the use of the `disable` method.
    - Fixed a minor formatting issue in the static file data example.
    - Bumped version number to 3.0.1 in package.json to reflect the new changes.

---
+++ docs/js/examples/disable.js
@@ -0,0 +1,28 @@
+const autocomplete = new Autocomplete("disable", {
+  onSearch: ({ currentValue }) => {
+    // local data
+    const data = [
+      { name: "Walter White" },
+      { name: "Jesse Pinkman" },
+      { name: "Skyler White" },
+      { name: "Walter White Jr." },
+    ];
+    return data
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .filter((element) => {
+        return element.name.match(new RegExp(currentValue, "i"));
+      });
+  },
+
+  onSubmit: () => {
+    // keep input value
+    autocomplete.disable();
+    // or
+    // remove input value
+    // autocomplete.disable(true);
+    // element.setAttribute("readonly", "readonly");
+  },
+
+  onResults: ({ matches }) =>
+    matches.map((el) => `<li>${el.name}</li>`).join(""),
+});

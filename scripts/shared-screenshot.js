a85d8f0c14313015f28a5ff72895c72a121443c9
Grzegorz Tomicki <gtomik@gmail.com>
2026-03-05 11:11:13 +0100

    refactor: switch to node module syntax for fs and path imports in shared-screenshot.js

--- scripts/shared-screenshot.js
+++ scripts/shared-screenshot.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import path from "node:path";
 
 export function getScreenshotPath(fileOrName, maybeName) {
   const today = new Date();
